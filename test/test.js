426089def2677e90a47a5555e92685ad5deb7af9
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-01-29 08:54:45 -0500

    Update

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ vm.runInThisContext(code);
 
 var should = require('chai').should();
 
-describe.('Player Moves', function() {
+describe('Player Moves', function() {
   it('a global variable called playerOneMoveOneType should exist and initialize to undefined', function() {
     should.equal(playerOneMoveOneType, undefined);
   });
@@ -57,7 +57,7 @@ describe.('Player Moves', function() {
   });
 });
 
-describe.only('setPlayerMoves() - Main Functionality', function() {
+describe('setPlayerMoves() - Main Functionality', function() {
   afterEach(clearMoves);
 
   it('a function called setPlayerMoves should exist', function() {
@@ -178,7 +178,7 @@ describe('setPlayerMoves() - Edge Cases', function() {
   });
 });
 
-describe('getRoundWinner() - Main Functionality', function() {
+describe.only('getRoundWinner() - Main Functionality', function() {
   it('a function called getRoundWinner should exist', function() {
     should.equal(typeof getRoundWinner, 'function');
   });
